3f30fe53113450a84c519cd9b29edb5b2880d72a
eva <evapapazisi@gmail.com>
2024-02-08 23:11:06 +0200

    update

--- script.js
+++ script.js
@@ -77,13 +77,11 @@ form.addEventListener("submit", async (event) => {
       console.log(description + " in showWeather ")
       const humidity = data.main.humidity;
       const imgsrc = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-      const date_time = getDateTime(data.dt, data.timezone);
   
       weatherDiv.innerHTML = `
     <h2 class="mb-3"><svg class="loc-icon"  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-fill" viewBox="0 0 16 16">
     <path fill-rule="evenodd" d="M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999zm2.493 8.574a.5.5 0 0 1-.411.575c-.712.118-1.28.295-1.655.493a1.319 1.319 0 0 0-.37.265.301.301 0 0 0-.057.09V14l.002.008a.147.147 0 0 0 .016.033.617.617 0 0 0 .145.15c.165.13.435.27.813.395.751.25 1.82.414 3.024.414s2.273-.163 3.024-.414c.378-.126.648-.265.813-.395a.619.619 0 0 0 .146-.15.148.148 0 0 0 .015-.033L12 14v-.004a.301.301 0 0 0-.057-.09 1.318 1.318 0 0 0-.37-.264c-.376-.198-.943-.375-1.655-.493a.5.5 0 1 1 .164-.986c.77.127 1.452.328 1.957.594C12.5 13 13 13.4 13 14c0 .426-.26.752-.544.977-.29.228-.68.413-1.116.558-.878.293-2.059.465-3.34.465s-2.462-.172-3.34-.465c-.436-.145-.826-.33-1.116-.558C3.26 14.752 3 14.426 3 14c0-.599.5-1 .961-1.243.505-.266 1.187-.467 1.957-.594a.5.5 0 0 1 .575.411z"/>
     </svg> ${cityName}</h2>
-    <h4 class="mb-3">${date_time}</h4>
     <img class="weather-icon style="width: 50px; margin-bottom: 10px;"" src="${imgsrc}">
     <div class="weather-content">
     <p>${temperature} with ${description} </p>
@@ -97,21 +95,21 @@ form.addEventListener("submit", async (event) => {
   }
   
 
-function getDateTime(dt, timezone) {
-  const date = new Date((dt + timezone) * 1000);
-  const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    hour12: false,
-    timeZoneOffset: timezone,
-  };
-  const dateTimeStr = date.toLocaleString(undefined, options);
-  return dateTimeStr;
-}
+// function getDateTime(dt, timezone) {
+//   const date = new Date((dt + timezone) * 1000);
+//   const options = {
+//     weekday: "long",
+//     year: "numeric",
+//     month: "long",
+//     day: "numeric",
+//     hour: "numeric",
+//     minute: "numeric",
+//     hour12: false,
+//     timeZoneOffset: timezone,
+//   };
+//   const dateTimeStr = date.toLocaleString(undefined, options);
+//   return dateTimeStr;
+// }
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -196,42 +194,51 @@ function mapWeatherToMood(weatherData) {
 
 // Example usage
 async function main(weatherData) {
-    const accessToken = await getAccessToken();
-    const mood = mapWeatherToMood(weatherData);
-    console.log(mood)
-    const recommendations = await fetchSpotifyRecommendations(accessToken, mood);
-    const songsList = document.createElement("ul");
-    console.log(recommendations)
-    // Limit the number of songs to 3
-    
-    recommendations.forEach(song => {
-     console.log("entered loop")
-        const listItem = document.createElement("li");
+  const accessToken = await getAccessToken();
+  const mood = mapWeatherToMood(weatherData);
+  console.log(mood);
+  const recommendations = await fetchSpotifyRecommendations(accessToken, mood);
+  const songsList = document.createElement("div");
+  songsList.classList.add("row");
+  songsList.classList.add("d-flex")
+  console.log(recommendations);
+  
+  recommendations.slice(0, 3).forEach(song => { // Use slice to limit to 3 songs
+      console.log("entered loop");
+      const listItem = document.createElement("div");
+      listItem.classList.add("col-sm");
+      listItem.classList.add("text-left");
+      listItem.classList.add("justify-content-center")
 
-        // Create elements for song name, artist, and album art
-        const songName = document.createElement("p");
-        songName.textContent = `Name: ${song.name}`;
 
-        const artist = document.createElement("p");
-        artist.textContent = `Artist: ${song.artists[0].name}`;
- 
-        const albumArt = document.createElement("img");
-        albumArt.src = song.album.images[0].url;
-        albumArt.alt = `Album art for ${song.name}`;
+      // Create elements for song name, artist, and album art
+      const songName = document.createElement("p");
+      songName.textContent = `${song.name}`;
 
-        // Append elements to list item
-        listItem.appendChild(songName);
-        listItem.appendChild(artist);
-        listItem.appendChild(albumArt);
+      const artist = document.createElement("p");
+      artist.textContent = `${song.artists[0].name}`;
 
-        // Append list item to songs list
-        songsList.appendChild(listItem);
-    });
-    clearRecommendations()
-    songsDiv.appendChild(songsList);
-    console.log("showed songs")
+      const albumArt = document.createElement("img");
+      albumArt.classList.add("song-img")
+      albumArt.src = song.album.images[0].url;
+      albumArt.alt = `Album art for ${song.name}`;
 
+      // Append elements to list item
+      
+      listItem.appendChild(albumArt);
+      listItem.appendChild(songName);
+      listItem.appendChild(artist);
+
+      // Append list item to songs list
+      songsList.appendChild(listItem);
+  });
+
+  clearRecommendations();
+  songsDiv.classList.remove("d-none");
+  songsDiv.appendChild(songsList);
+  console.log("showed songs");
 }
+
 
 
   function clearRecommendations() {
