79495c1c5bb2273d5324129185474b20cd584d08
eva <evapapazisi@gmail.com>
2024-02-14 18:07:20 +0200

    finished styling

--- script.js
+++ script.js
@@ -25,91 +25,83 @@ async function getAccessToken() {
 }
 
 // Call the main function after fetching weather data
+// form.addEventListener("submit", async (event) => {
+// 	event.preventDefault();
+// 	const cityName = input.value.trim();
+// 	const weatherData = await getWeather(cityName);
+// 	main(weatherData);
+//   });
+// Call the main function after fetching weather data
 form.addEventListener("submit", async (event) => {
-	event.preventDefault();
-	const cityName = input.value.trim();
-	const weatherData = await getWeather(cityName);
-	main(weatherData);
-  });
- 
-  
-  async function getWeather(cityName) {
-	const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
-	try {
-	  const response = await fetch(url);
-	  const data = await response.json();
-	  showWeather(data);
-	  // mapWeatherToMood(data);
-	  return data; // Return weather data
-	} catch (error) {
-	  console.log("Error:", error);
-	}
-  }
-  
-  function showWeather(data) {
-    console.log("in showWeather")
-    if (!data || !data.main || !data.main.temp || !data.weather || !data.weather[0] || !data.weather[0].description) {
-      console.error("Invalid weather data:", data);
-      return; // Exit early if data is invalid or missing properties
-    }
-  
-    if (data.cod === "404") {
-      // show error message and hide weather data
-      weatherDiv.innerHTML = `
-    <div class="weather-content">
-    <p id="error-message" style="display: block;">404 ERROR<br>City not found</p>
-    </div`;
-    } else {
-      const cityName = data.name;
-  
-      // Check if 'isCelsius' is defined before using it
-      let temperature;
-      let feels_like;
-      if (isCelsius) {
-        temperature = data.main.temp + "°C";
-        feels_like = data.main.feels_like + "°C";
-      } else {
-        temperature = (data.main.temp * 9) / 5 + 32 + "°F";
-        feels_like = (data.main.feels_like * 9) / 5 + 32 + "°F";
-      }
-  
-      const description = data.weather[0].description;
-      console.log(description + " in showWeather ")
-      const humidity = data.main.humidity;
-      const imgsrc = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-  
-      weatherDiv.innerHTML = `
-    <h2 class="mb-3"><svg class="loc-icon"  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-fill" viewBox="0 0 16 16">
-    <path fill-rule="evenodd" d="M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999zm2.493 8.574a.5.5 0 0 1-.411.575c-.712.118-1.28.295-1.655.493a1.319 1.319 0 0 0-.37.265.301.301 0 0 0-.057.09V14l.002.008a.147.147 0 0 0 .016.033.617.617 0 0 0 .145.15c.165.13.435.27.813.395.751.25 1.82.414 3.024.414s2.273-.163 3.024-.414c.378-.126.648-.265.813-.395a.619.619 0 0 0 .146-.15.148.148 0 0 0 .015-.033L12 14v-.004a.301.301 0 0 0-.057-.09 1.318 1.318 0 0 0-.37-.264c-.376-.198-.943-.375-1.655-.493a.5.5 0 1 1 .164-.986c.77.127 1.452.328 1.957.594C12.5 13 13 13.4 13 14c0 .426-.26.752-.544.977-.29.228-.68.413-1.116.558-.878.293-2.059.465-3.34.465s-2.462-.172-3.34-.465c-.436-.145-.826-.33-1.116-.558C3.26 14.752 3 14.426 3 14c0-.599.5-1 .961-1.243.505-.266 1.187-.467 1.957-.594a.5.5 0 0 1 .575.411z"/>
-    </svg> ${cityName}</h2>
-    <img class="weather-icon style="width: 50px; margin-bottom: 10px;"" src="${imgsrc}">
-    <div class="weather-content">
-    <p>${temperature} with ${description} </p>
-    <p>feels like ${feels_like} </p>
-    <p>${humidity}% humidity</p>
-    </div>
-    `;
-  
-      weatherDiv.style.display = "block";
-    }
-  }
-  
+  event.preventDefault();
+  const cityName = event.target.elements["city"].value.trim(); // Adjust "city" to match the name attribute of your input field
+  console.log(cityName)
+  const weatherData = await getWeather(cityName);
+  main(weatherData);
+});
 
-// function getDateTime(dt, timezone) {
-//   const date = new Date((dt + timezone) * 1000);
-//   const options = {
-//     weekday: "long",
-//     year: "numeric",
-//     month: "long",
-//     day: "numeric",
-//     hour: "numeric",
-//     minute: "numeric",
-//     hour12: false,
-//     timeZoneOffset: timezone,
-//   };
-//   const dateTimeStr = date.toLocaleString(undefined, options);
-//   return dateTimeStr;
-// }
+ 
+  async function getWeather(cityName) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      showWeather(data);
+      // mapWeatherToMood(data);
+      return data; // Return weather data
+    } catch (error) {
+      console.log("Error:", error);
+    }
+    }
+
+    function showWeather(data) {
+      console.log("in showWeather")
+      if (!data || !data.main || !data.main.temp || !data.weather || !data.weather[0] || !data.weather[0].description) {
+        console.error("Invalid weather data:", data);
+        return; // Exit early if data is invalid or missing properties
+      }
+    
+      if (data.cod === "404") {
+        // show error message and hide weather data
+        weatherDiv.innerHTML = `
+      <div class="weather-content">
+      <p id="error-message" style="display: block;">404 ERROR<br>City not found</p>
+      </div`;
+      } else {
+        const cityName = data.name;
+    
+        // Check if 'isCelsius' is defined before using it
+        let temperature;
+        let feels_like;
+        if (isCelsius) {
+          temperature = data.main.temp + "°C";
+          feels_like = data.main.feels_like + "°C";
+        } else {
+          temperature = (data.main.temp * 9) / 5 + 32 + "°F";
+          feels_like = (data.main.feels_like * 9) / 5 + 32 + "°F";
+        }
+    
+        const description = data.weather[0].description;
+        console.log(description + " in showWeather ")
+        const humidity = data.main.humidity;
+        const imgsrc = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    
+        weatherDiv.innerHTML = `
+      <h2 class="mb-3"><svg class="loc-icon"  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-fill" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999zm2.493 8.574a.5.5 0 0 1-.411.575c-.712.118-1.28.295-1.655.493a1.319 1.319 0 0 0-.37.265.301.301 0 0 0-.057.09V14l.002.008a.147.147 0 0 0 .016.033.617.617 0 0 0 .145.15c.165.13.435.27.813.395.751.25 1.82.414 3.024.414s2.273-.163 3.024-.414c.378-.126.648-.265.813-.395a.619.619 0 0 0 .146-.15.148.148 0 0 0 .015-.033L12 14v-.004a.301.301 0 0 0-.057-.09 1.318 1.318 0 0 0-.37-.264c-.376-.198-.943-.375-1.655-.493a.5.5 0 1 1 .164-.986c.77.127 1.452.328 1.957.594C12.5 13 13 13.4 13 14c0 .426-.26.752-.544.977-.29.228-.68.413-1.116.558-.878.293-2.059.465-3.34.465s-2.462-.172-3.34-.465c-.436-.145-.826-.33-1.116-.558C3.26 14.752 3 14.426 3 14c0-.599.5-1 .961-1.243.505-.266 1.187-.467 1.957-.594a.5.5 0 0 1 .575.411z"/>
+      </svg> ${cityName}</h2>
+      <img class="weather-icon style="width: 50px; margin-bottom: 10px;"" src="${imgsrc}">
+      <div class="weather-content">
+      <p>${temperature} with ${description} </p>
+      <p>feels like ${feels_like} </p>
+      <p>${humidity}% humidity</p>
+      </div>
+      `;
+    
+        weatherDiv.style.display = "block";
+      }
+    }
+    
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -122,13 +114,12 @@ function getLocation() {
 function showPosition(position) {
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
-  console.log(latitude, longitude);
-  // Do something with the user's latitude and longitude
+
   getWeatherLatLon(latitude, longitude);
 }
 
 async function getWeatherLatLon(lat, lon) {
-  console.log(" in getWeatherLatLon")
+  console.log(" in getWeatherLatLon");
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
 
   try {
@@ -136,9 +127,7 @@ async function getWeatherLatLon(lat, lon) {
     const data = await response.json();
     showWeather(data);
 
-    main(data)
-    console.log(data)
-
+    main(data);
   } catch (error) {
     console.log("Error:", error);
   }
@@ -198,22 +187,30 @@ async function main(weatherData) {
   const mood = mapWeatherToMood(weatherData);
   console.log(mood);
   const recommendations = await fetchSpotifyRecommendations(accessToken, mood);
-   console.log(recommendations[0].album.images[0].url);
-  const songImg=document.querySelector(".song-img");
-  songImg.src=recommendations[0].album.images[0].url;
-  songImg.alt=`Album art for ${recommendations[0].name}`;
-  const info=document.querySelector(".song-info");
-  info.textContent=` ${recommendations[0].name} by ${recommendations[0].artists[0].name}`;
-
-  
-  songsDiv.classList.remove("d-none");
-  console.log("showed songs");
-};
-
-
-
-  function clearRecommendations() {
-	console.log("Cleared")
-	songsDiv.innerHTML = ''; // Clear the inner HTML of songsDiv
+  console.log(recommendations[0].album.images[0].url);
+  const songImg = document.getElementById("cover");
+  songImg.src = recommendations[0].album.images[0].url;
+  songImg.alt = `Album art for ${recommendations[0].name}`;
+  const songTitle = document.getElementById("title");
+  songTitle.textContent = ` ${recommendations[0].name}`;
+  const songArtist = document.getElementById("artist");
+  songArtist.textContent = `by ${recommendations[0].artists[0].name}`;
+  const duration = document.getElementById("time");
+  duration.textContent =
+    Math.floor((recommendations[0].duration_ms / 1000 / 60) % 60) + `:00`;
+  const audioPlayer = document.getElementById("audio-player");
+  console.log(recommendations[0].external_urls);
+  const pauseButton = document.getElementById("pause-btn");
+  audioPlayer.src = recommendations[0].preview_url;
+  const spotLink=document.getElementById("spotify-btn")
+  spotLink.href= recommendations[0].external_urls.spotify;
+  const text=document.getElementById("song-text");
+  text.textContent=`a ${mood} song to match a ${weatherData.weather[0].description.toLowerCase()} day..`;
+  if (audioPlayer.src == null) {
+    document.querySelector("error-text").textContent =
+      "Sorry! There is no preview available";
   }
-  
+  pauseButton.addEventListener("click", function () {
+    audioPlayer.paused ? audioPlayer.play() : audioPlayer.pause();
+  });
+}
